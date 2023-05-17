b43c7a72aa159b27b5d24d4f7860434a4dceae31
JimmyShukurow <jimishukurow@gmail.com>
2023-05-17 06:56:52 +0300

    Tic Tac Toe is almost done

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -24,14 +24,19 @@ window.onload = ()=> {
 }
 let playerXIcon = "fas fa-times";
 let playerOIcon = "far fa-circle";
+let playerSign = "X";
+
 
 function clickedBox(element){
     if(players.classList.contains("player")){
         element.innerHTML = `<i class="${playerOIcon}"><i>`;
         players.classList.add("active");
+        playerSign = "O";
+        element.setAttribute("id", playerSign);
     } else {
         element.innerHTML = `<i class="${playerXIcon}"><i>`;
         players.classList.add("active");
+        element.setAttribute("id", playerSign)
     }
 
     element.style.pointerEvents = "none";
@@ -44,23 +49,27 @@ function clickedBox(element){
 
 
 function bot(){
+    playerSign = "O"
     let array = [];
     for (let i = 0; i < allBox.length; i++) {
         if (allBox[i].childElementCount == 0) {
             array.push(i);
-            console.log(i + " has no children");
         }        
     }
     let randomBox = array[Math.floor(Math.random()*array.length)];
-    console.log(randomBox);
     if (array.length > 0 ) {
         if(players.classList.contains("player")){
             allBox[randomBox].innerHTML = `<i class="${playerXIcon}"><i>`;
-            players.classList.add("active");
+            players.classList.remove("active");
+            playerSign="X";
+            allBox[randomBox].setAttribute("id", playerSign);
         } else {
             allBox[randomBox].innerHTML = `<i class="${playerOIcon}"><i>`;
-            players.classList.add("active");
+            players.classList.remove("active");
+            allBox[randomBox].setAttribute("id", playerSign);
         }
     }
-    // console.log(array);
+
+    allBox[randomBox].style.pointerEvents = "none"
+
 }
